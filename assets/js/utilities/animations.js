254ca3a02e5ef7a69fcaa900ee88f7d3a97bfae9
MrHidalgo <Luckyhidalgo@gmail.com>
2024-08-21 20:12:37 +0300

    Initial commit

--- assets/js/utilities/animations.js
+++ assets/js/utilities/animations.js
@@ -1,13 +1,16 @@
 // Animate on scroll
 const animations = document.querySelectorAll('[data-aos]');
-const observedClass = 'js-intersection-observed';
+
+// const observedClassAdd = 'js-intersection-observed';
+// const observedClassRemove = 'js-zoom';
+
 const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
         const index = Array.from(animations).indexOf(entry.target);
         if (entry.isIntersecting) {
-            animations[index].classList.add(observedClass);
+            animations[index].classList.add('js-intersection-observed', 'js-zoom');
         } else {
-            animations[index].classList.remove(observedClass);
+            animations[index].classList.remove('js-zoom');
         }
     })
 }, {
